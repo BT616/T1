1287b0897fd74ad8eb426ffc4f744509468f0f0e
BT616 <110855674+BT616@users.noreply.github.com>
2023-04-25 15:07:57 -0700

    next button

--- script.js
+++ script.js
@@ -1,31 +1,69 @@
 var button = document.getElementById('btn')
-var nextBtn= document.createElement('button')
-
+var nextBtn = document.createElement('button')
 var form = document.getElementById('form');
 
 
+var url = 'https://jsonplaceholder.typicode.com'
+
+async function start() {
+  var response = await fetch(`${url}/posts/1`);
+  var data = await response.json()
+
+  document.getElementById("display").innerHTML = data.title;
+  console.log(data)
+
+  function next() {
+    nextBtn.innerHTML = ">"
+    form.appendChild(nextBtn);
+  } next()
 
 
 
-
-
- async function start(){
-   var response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-      var data = await response.json()
-      
-      console.log(data)
-
-      function next(){
-      nextBtn.innerHTML= ">"
-      form.appendChild(nextBtn)
-    } next()
-
-     document.getElementById("display").innerHTML = data.title; 
 };
 
 
+// nextBtn.addEventListener('click', function event(){
+nextBtn.addEventListener('click', increment)
+
+
+
+function increment() {
+
+  var count = 1;
+  nextBtn.onclick = function() {
+    count ++;
+nextBtn.innerHTML = count
+}
+
+  var nextQuote = `${url}/posts/${count}`
+
+
+
+  console.log(count)
+
+  fetch(nextQuote).then(function (response) {
+    return response.json()
+
+  }).then(function (data) {
+    console.log(data.title)
+    if (data.results === 0) {
+      console.log("No results.");
+    } else {
+      document.getElementById("display").innerHTML = data.body;
+    }
+  });
+
+  //     .then(function (data) {
+  //       if (data.length === 0) {
+  //           console.log("No results.");
+  //       } else {
+  //         console.log('thank you')
+  //       }
+  // })
+}
 
 
 
 
-button.addEventListener("click",start)
+
+button.addEventListener("click", start)
